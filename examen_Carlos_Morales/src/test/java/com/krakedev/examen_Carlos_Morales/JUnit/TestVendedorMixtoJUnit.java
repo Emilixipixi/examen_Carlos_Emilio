1010327c19a6fde8972f13169d6b7e8a3602815f
Emilixipixi <carlosemilio270798@gmail.com>
2026-05-09 21:51:24 -0500

    Vendedor Mixto y su test unitario agregados

---
+++ examen_Carlos_Morales/src/test/java/com/krakedev/examen_Carlos_Morales/JUnit/TestVendedorMixtoJUnit.java
@@ -0,0 +1,25 @@
+package com.krakedev.examen_Carlos_Morales.JUnit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import com.krakedev.examen_Carlos_Morales.VendedorMixto;
+
+public class TestVendedorMixtoJUnit {
+
+	@Test
+	public void deberiaCalcularSueldoMixtoCorrectamente() {
+
+		VendedorMixto vendedor = new VendedorMixto("1002");
+
+		vendedor.setSueldoFijo(500);
+		vendedor.setNumeroVentas(4);
+
+		double resultado = vendedor.calcularSueldo();
+
+		assertEquals(520, resultado);
+
+	}
+
+}
