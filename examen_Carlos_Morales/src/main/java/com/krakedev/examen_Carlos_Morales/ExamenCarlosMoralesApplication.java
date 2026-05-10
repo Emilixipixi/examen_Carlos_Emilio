e130c94aea5cf230708f23a6eb72a875d21c1e6d
Emilixipixi <carlosemilio270798@gmail.com>
2026-05-09 21:07:29 -0500

    first commit

---
+++ examen_Carlos_Morales/src/main/java/com/krakedev/examen_Carlos_Morales/ExamenCarlosMoralesApplication.java
@@ -0,0 +1,13 @@
+package com.krakedev.examen_Carlos_Morales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenCarlosMoralesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenCarlosMoralesApplication.class, args);
+	}
+
+}
---
+++ examen_Carlos_Morales/src/test/java/com/krakedev/examen_Carlos_Morales/ExamenCarlosMoralesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krakedev.examen_Carlos_Morales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenCarlosMoralesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
